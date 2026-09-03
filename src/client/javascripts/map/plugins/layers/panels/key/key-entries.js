b3b287939b43feff80acd8b9ce208164d1e5aa9e
Jordon Smith <jordon.smith@informed.com>
2026-09-03 12:34:04 +0100

    feat(GEP-1412): update KeyPanel to support cloud native datasets (#115)
    - Split out unrelated CSS from _layers.scss after refactor

--- src/client/javascripts/map/plugins/layers/panels/key/key-entries.js
+++ src/client/javascripts/map/plugins/layers/panels/key/key-entries.js
@@ -1,9 +1,30 @@
 import { datasetForLayer } from '../../../../config/layers.js'
 import { UNKNOWN_LAYER_LABEL } from '../../constants.js'
 import { getSourceUrl, getVisibleWmsLayers } from '../../datasets/layers/wms.js'
+import { hasVisibleFill, hasVisibleStroke } from '../../datasets/style-config.js'
+
+function stylesForKey (styleConfig) {
+  return [...styleConfig.classes, styleConfig.default]
+    .filter(definition => hasVisibleFill(definition) || hasVisibleStroke(definition))
+}
+
+function getVisibleStyleEntries (map, datasets) {
+  const visibleDatasets = map.getLayers().getArray()
+    .filter(layer => layer.getVisible())
+    .map(layer => datasetForLayer(layer, datasets))
+    .filter(dataset => dataset?.source?.styleConfig)
+
+  // Detail and overview layers map to the same dataset.
+  return [...new Set(visibleDatasets)]
+    .map(dataset => ({
+      label: dataset.label,
+      styles: stylesForKey(dataset.source.styleConfig)
+    }))
+    .filter(entry => entry.styles.length)
+}
 
 export function getKeyEntries (map, datasets) {
-  return getVisibleWmsLayers(map).map(layer => {
+  const wmsEntries = getVisibleWmsLayers(map).map(layer => {
     const dataset = datasetForLayer(layer, datasets)
     const label = dataset?.label ?? UNKNOWN_LAYER_LABEL
     const source = layer.getSource()
@@ -14,4 +35,6 @@ export function getKeyEntries (map, datasets) {
     }
     return { label, baseUrl, layerNames: layerNames.split(',') }
   }).filter(Boolean)
+
+  return [...getVisibleStyleEntries(map, datasets), ...wmsEntries]
 }
